2da1140a75842167e8456e0a89e3e7bea7efc1ca
mujju <mohdmujahed1716@gmail.com>
2026-01-17 16:02:52 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "local" {}
+provider "random" {}
